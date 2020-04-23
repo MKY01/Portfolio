198f80b151a08dda1ed713d7da3f12dfe9093893
MKY01 <myip01@mail.bbk.ac.uk>
2020-04-23 23:14:26 +0100

    Increase size of title to make it more prominent

--- JavaScript/kodflix/src/App.js
+++ JavaScript/kodflix/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Welcome to Kodflix!
+            <h1>Welcome to Kodflix!</h1>
           </p>
           <a
             className="App-link"
